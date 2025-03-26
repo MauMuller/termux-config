56dd96e10ee7a99f689c81a01e1ab2b6ce9989a4
MauMuller <mauriciomuller@gmail.com>
2025-03-25 23:47:29 -0300

    (feat) Starting termux configurations CLI
    My ideia is attemping to automate termux configurations to become easier
    to change, but, for now, i just did a switches to treat invalid commands
    with a inicial template to organize multiples options to be inserted on
    shell script.

---
+++ termux-shortcuts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+name=$(echo $0 | sed -E "s/(^\.\\/)|(\.\w+$)//gm")
+
+invalidCommand () {
+	invalid=$1
+	
+	validCommand=$(if [ $2 ]; then echo $2; else echo "<command>"; fi)
+	validOptions=$(if [ $3 ]; then echo $3; else echo "[options]"; fi)
+
+	echo -e "\nUsage:"
+	echo -e "  $name $validCommand $validOptions"
+
+	echo -e "\nno such option: $invalid"
+}
+
+case $1 in
+	"" | --help | -h )
+		echo "help"
+		;;
+
+	visibillity )
+		case $2 in
+			on )
+				echo "get"
+				;;
+			off )
+				echo "set"
+				;;
+
+			"" )
+				echo "help visibillity"
+				;;
+			* )
+				invalidCommand $2 "visibilitty"
+				;;
+		esac
+		;;
+
+	* )
+		invalidCommand $1
+		;;
+esac
